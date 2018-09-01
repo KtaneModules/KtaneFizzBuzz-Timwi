6f8caec82a36c97140637406b45dda3cbda3b850
Matthew McCaskill <mattrmccaskill@gmail.com>
2018-09-01 14:40:21 -0500

    Rule Seed Modifier support

---
+++ Assets/Modules/FizzBuzz/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extensions {
+    // Fisher-Yates Shuffle
+    public static IList<T> Shuffle<T>(this IList<T> list, MonoRandom rnd) {
+        int i = list.Count;
+        while (i > 1) {
+            int index = rnd.Next(i);
+            i--;
+            T value = list[index];
+            list[index] = list[i];
+            list[i] = value;
+        }
+        return list;
+    }
+}
